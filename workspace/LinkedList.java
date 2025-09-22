e93fd35bdce228b0cffc512d6b82ae6504b8ebc9
JCAtlas08 <campjosh21@gmail.com>
2025-09-22 23:31:50 +0000

    yay I did it

--- workspace/LinkedList.java
+++ workspace/LinkedList.java
@@ -14,39 +14,62 @@ Problem:  Write a program that keeps and manipulates a linked list of
 	Output:  the results to the screen of each menu
 	    choice, and error messages where appropriate.
 */
-public class LinkedList{
-
-  //instance varialbes go here (think about what you need to keep track of!)
+public class LinkedList {
+  //instance variables go here (think about what you need to keep track of!)
+  ListNode head;
 
   //constructors go here
+  public LinkedList() {
+    head = new ListNode(null, null);
+  }
 
+  public ListNode placeNode(String value, ListNode node) {
+    if (node.getValue().compareTo(value) > 0) {
+      placeNode(value, node.getNext());
+    }
+    node = new ListNode(value, node);
+    return node;
+  }
 
   //precondition: the list has been initialized
   //postcondition: the ListNode containing the appropriate value has been added and returned
-  public ListNode addAValue(String line)
-  {
-    return null;
+  public ListNode addAValue(String line) {
+    return placeNode(count, line, head);
+  }
+
+  public ListNode removeNode(String value, ListNode node) {
+    ListNode next = node.getNext();
+    if (node.getValue() == value) {
+      removeNode(value, next);
+    }
+    node = new ListNode(next.getValue(), next);
+    return node;
   }
 
   //precondition: the list has been initialized
   //postcondition: the ListNode containing the appropriate value has been deleted and returned.
   //if the value is not in the list returns null
-  public ListNode deleteAValue(String line)
-  {
-    return null;
+  public ListNode deleteAValue(String line) {
+    return removeNode(line, head);
+  }
+
+  public String allNodes(ListNode node) {
+    if (node.getNext() == null) {
+      return node.getValue();
+    }
+    return node.getValue() + " " + allNodes(node.getNext());
   }
 
   //precondition: the list has been initialized
   //postconditions: returns a string containing all values appended together with spaces between.
-  public String showValues()
-  {
-    return null;
+  public String showValues() {
+    return allNodes(head);
   }
 
   //precondition: the list has been initialized
   //postconditions: clears the list.
   public void clear()
   {
-  
+    head = new ListNode(null, null);
   }
 }
--- workspace/ListNode.java
+++ workspace/ListNode.java
@@ -7,50 +7,37 @@ public class ListNode
 	
 	
 	// constructor
-
-
 	// pre-condition: initValue is a valid Object and initNext is valid ListNode.
 	// post-condition: value equals initValue and next equals initNext.
-	public ListNode (String initValue,
-					 ListNode initNext)
-	{
+	public ListNode (String initValue, ListNode initNext) {
 		value = initValue;
 		next = initNext;
 		return;
 	}
 	
-	
 	// accessors
-
 	// pre-condition: value is either null or a valid Object.
 	// post-condition: value is returned.
-	public String getValue()
-	{
+	public String getValue() {
 		return value;
 	}
 
-
 	// pre-condition: next is either null or a valid ListNode.
 	// post-condition: next is returned.
-	public ListNode getNext ()
-	{
+	public ListNode getNext() {
 		return next;
 	}
-	
-	
+
 	// mutators
-	
 	// pre-condition: theNewValue is a valid Object.
 	// post-condition: value equals theNewValue.
-	public void setValue (String theNewValue)
-	{
+	public void setValue (String theNewValue) {
 		value = theNewValue;
 	}
 
 	// pre-condition: theNewNext is a valid ListNode.
 	// post-condition: next equals theNewNext.
-	public void setNext(ListNode theNewNext)
-	{
+	public void setNext(ListNode theNewNext) {
 		next = theNewNext;
 	}
 }
